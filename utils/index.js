4875f3a9bd0259b1c5cfb3519acacd87aac8cf44
badermah <bmahdawi1@student.rccd.edu>
2023-05-11 19:54:46 -0700

    Fixed DOTENV require

--- utils/index.js
+++
@@ -1,8 +0,0 @@
-const { DateTime } = require('luxon');
-
-module.exports = {
-  format_date: (date) => {
-    return DateTime.fromJSDate(date).toFormat('ff');
-  },
-  toUpperCase: (str) => str.toUpperCase(),
-};
