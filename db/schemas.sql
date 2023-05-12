19d5d00ef6b9de64922e5b480385a67e6dc57c89
Frank-Colorado <chawnkiee@gmail.com>
2023-05-11 21:24:07 -0400

    created users table in schemas.sql

--- db/schemas.sql
+++ db/schemas.sql
@@ -1,2 +1,14 @@
 DROP DATABASE IF EXISTS rich_lifestyle;
 CREATE DATABASE rich_lifestyle;
+
+USE rich_lifestyle;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR (15) NOT NULL,
+    password VARCHAR (20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+
