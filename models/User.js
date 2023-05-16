426adca2ae2a5c6def98a68370610bd807469b20
Frank-Colorado <chawnkiee@gmail.com>
2023-05-15 22:14:32 -0400

    created 'checkPassword' instance method

--- models/User.js
+++ models/User.js
@@ -1,4 +1,12 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class User extends Model {}
+class User extends Model {
+  // This is an instance method called 'checkPassword'
+  // It has 1 param called 'userPassword'
+  // It's purpose is to check the userPassword against the hashedPassword
+  async checkPassword(userPassword) {
+    try {
+    } catch (err) {}
+  }
+}
