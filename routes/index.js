73d5dc9b0b4770fdc982eb8b636af34b3a557f3a
Frank-Colorado <chawnkiee@gmail.com>
2023-05-15 20:56:18 -0400

    changed routing in router index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const homeRoutes = require("./homeRoutes");
+const htmlRoutes = require("./htmlRoutes");
 const apiRoutes = require("./apiRoutes");
 
-router.use("/", homeRoutes);
+router.use("/", htmlRoutes);
 router.use("/api", apiRoutes);
+
 module.exports = router;
