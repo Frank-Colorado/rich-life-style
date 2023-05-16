9682a907ff538f0d3e7119857e4c7d25ab91544c
Frank-Colorado <chawnkiee@gmail.com>
2023-05-16 19:55:21 -0400

    added comments in userRoutes and added /signup

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -5,8 +5,11 @@ const {
   deleteUser,
 } = require("../../../controllers/userController");
 
-router.post("/", createUser);
+// This route will be called with /api/users/signup
+router.post("/signup", createUser);
+// This route will be called with /api/users/:id (PUT)
 router.put("/:id", updateUser);
+// This route will be called with /api/users/:id (DELETE)
 router.delete("/:id", deleteUser);
 
 module.exports = router;
