b15d472a283bbf64cc938d64a89257baf8cb50de
Frank-Colorado <chawnkiee@gmail.com>
2023-05-15 22:44:19 -0400

    Created User table in User.js

--- models/User.js
+++ models/User.js
@@ -10,3 +10,31 @@ class User extends Model {
     } catch (err) {}
   }
 }
+
+User.init(
+  {
+    // columns will go here
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [8] },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: "user",
+  }
+);
