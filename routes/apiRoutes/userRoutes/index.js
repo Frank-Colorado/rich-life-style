b9f7ec9f621e09191f9b038bdd703b5d064ebee5
Frank-Colorado <chawnkiee@gmail.com>
2023-05-16 20:57:30 -0400

    created routes for login/logout in userRoutes

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,16 +1,24 @@
 const router = require("express").Router();
 const {
   createUser,
+  loginUser,
+  logoutUser,
   updateUser,
   deleteUser,
 } = require("../../../controllers/userController");
 
 // This route will be called with /api/users/signup
 router.post("/signup", createUser);
+
 // This route will be called with /api/users/login
 router.post("/login", loginUser);
+
+// This route will be called with /api/users/logout
+router.post("/logout", logoutUser);
+
 // This route will be called with /api/users/:id (PUT)
 router.put("/:id", updateUser);
+
 // This route will be called with /api/users/:id (DELETE)
 router.delete("/:id", deleteUser);
 
