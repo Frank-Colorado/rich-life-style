a8f775154d01e494c04e9e413526acc3046bc600
Frank-Colorado <chawnkiee@gmail.com>
2023-05-16 23:43:37 -0400

    added User.hasMany association with Post

--- models/index.js
+++ models/index.js
@@ -2,4 +2,11 @@ const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
 
+// User Associations with Post
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  foreignKey: "author",
+  onDelete: "CASCADE",
+});
+
 module.exports = { User, Post, Comment };
