c891c0787e8f67147fb96bf6715e0f9ffd3071c4
Frank-Colorado <chawnkiee@gmail.com>
2023-05-16 23:39:05 -0400

    created Comment Model in Model.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,2 +1,39 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: "comment",
+  }
+);
