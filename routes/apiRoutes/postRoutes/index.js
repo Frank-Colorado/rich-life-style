5e4693dd117da4a8c8d8c3e268dbea99037ee10e
badermah <bmahdawi1@student.rccd.edu>
2023-05-16 19:35:15 -0700

    Create post controller and routes

--- routes/apiRoutes/postRoutes/index.js
+++ routes/apiRoutes/postRoutes/index.js
@@ -1,2 +1,17 @@
 const router = require("express").Router();
+const {
+  createPost,
+  updatePost,
+  deletePost,
+} = require("../../../controllers/apiControllers/postController.js");
+
+// This route will be called with /api/posts (POST)
+router.post("/", createPost);
+
+// This route will be called with /api/posts/:id (PUT)
+router.put("/:id", updatePost);
+
+// This route will be called with /api/posts/:id (DELETE)
+router.delete("/:id", deletePost);
+
 module.exports = router;
