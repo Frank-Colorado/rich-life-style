8167bbdf5b9b0ac6eb9dedfae8fcfea457130a1d
Frank-Colorado <chawnkiee@gmail.com>
2023-05-18 23:22:58 -0400

    edited comments in some files

--- controllers/apiControllers/commentController.js
+++ controllers/apiControllers/commentController.js
@@ -24,10 +24,10 @@ const createComment = async (req, res) => {
   }
 };
 
-// This route will be called with /api/posts/:id (PUT)
+// This route will be called with /api/comment/:id (PUT)
 const updateComment = async (req, res) => {
   try {
-    // We update the post with the title and content that was entered in the form
+    // We update the comment with the content that was entered in the form
     const updatedComment = await Comment.update(
       {
         content: req.body.content,
@@ -38,7 +38,7 @@ const updateComment = async (req, res) => {
         },
       }
     );
-    // We send a response to the client with the post's information
+    // We send a response to the client with the updated comment
     res
       .status(200)
       .json({ comment: updatedComment, message: "Comment updated!" });
@@ -51,16 +51,16 @@ const updateComment = async (req, res) => {
   }
 };
 
-// This route will be called with /api/posts/:id (DELETE)
+// This route will be called with /api/comments/:id (DELETE)
 const deleteComment = async (req, res) => {
   try {
-    // We delete the post with the id that was sent to us in the params of the request
+    // We delete the comment with the id that was sent to us in the params of the request
     const deletedComment = await Comment.destroy({
       where: {
         id: req.params.id,
       },
     });
-    // We send a response to the client with the post's information
+    // We send a response to the client with the comment's information
     res
       .status(200)
       .json({ comment: deletedComment, message: "Comment deleted!" });
--- routes/apiRoutes/commentRoutes/index.js
+++ routes/apiRoutes/commentRoutes/index.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
 const {
-    createComment,
-    updateComment,
-    deleteComment,
+  createComment,
+  updateComment,
+  deleteComment,
 } = require("../../../controllers/apiControllers/commentController.js");
 
-// This route will be called with /api/posts (POST)
+// This route will be called with /api/comments (POST)
 router.post("/", createComment);
 
-// This route will be called with /api/posts/:id (PUT)
+// This route will be called with /api/comments/:id (PUT)
 router.put("/:id", updateComment);
 
-// This route will be called with /api/posts/:id (DELETE)
+// This route will be called with /api/comments/:id (DELETE)
 router.delete("/:id", deleteComment);
 
 module.exports = router;
