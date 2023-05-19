fd196136862f9bb7e314dfdec01a52854863dcc8
Frank-Colorado <chawnkiee@gmail.com>
2023-05-18 23:55:16 -0400

    created a dropdownBtn event function in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
 const $dropdownBtn = document.getElementById("dropdownBtn");
 const $dropdownMenu = document.getElementById("dropdownMenu");
 const $logoutBtn = document.getElementById("logoutBtn");
+
+// This is an event listener that listens for a click on the dropdown button
+$dropdownBtn.addEventListener("click", () => {
+  // We toggle the show class on the dropdown menu
+  $dropdownMenu.classList.toggle("show");
+});
