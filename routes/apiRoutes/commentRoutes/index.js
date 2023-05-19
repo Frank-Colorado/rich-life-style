31d8bced6ac522ffa276f004349624c9df61c216
badermah <bmahdawi1@student.rccd.edu>
2023-05-18 18:56:19 -0700

    Comment routes

--- routes/apiRoutes/commentRoutes/index.js
+++ routes/apiRoutes/commentRoutes/index.js
@@ -1,2 +1,17 @@
 const router = require("express").Router();
+const {
+    createComment,
+    updateComment,
+    deleteComment,
+} = require("../../../controllers/apiControllers/commentController.js");
+
+// This route will be called with /api/posts (POST)
+router.post("/", createComment);
+
+// This route will be called with /api/posts/:id (PUT)
+router.put("/:id", updateComment);
+
+// This route will be called with /api/posts/:id (DELETE)
+router.delete("/:id", deleteComment);
+
 module.exports = router;
