8222fe506c47a39046543dcdc1266dac671df102
Frank-Colorado <chawnkiee@gmail.com>
2023-05-18 22:49:37 -0400

    created variables in dashboard.js

---
+++ public/js/dashboard.js
@@ -0,0 +1,5 @@
+const $postForm = document.getElementById("postForm");
+const $postTitle = document.getElementById("postTitle");
+const $postContent = document.getElementById("postContent");
+const $postBtn = document.getElementById("postBtn");
+const $alert = document.getElementById("alert");
