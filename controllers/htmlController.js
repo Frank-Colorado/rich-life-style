62909418c07ce3709d8c575b7ef0170a4ab9bb33
Frank-Colorado <chawnkiee@gmail.com>
2023-05-18 22:34:07 -0400

    edited displayDash in htmlController.js

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -3,17 +3,10 @@ const { User, Post, Comment } = require("../models");
 // This is a function that displays the home page if the user is logged in, otherwise it redirects the user to the login page.
 const displayDash = async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findOne({
-      // We specify that we don't want the user's password to be returned
-      attributes: { exclude: ["password"] },
-      where: { id: req.session.user_id },
-    });
-    // We modify the userData so it can be used by handlebars
-    const user = userData.get({ plain: true });
-    // We render the user_profile with the user's data
+    // We render the user_profile with the user's data from the session
     res.render("Dashboard", {
-      user,
+      username: req.session.username,
+      loggedIn: req.session.logged_in,
       title: "Dashboard",
       heading: "User Dashboard",
     });
