60e4a772b780d9c241610d53b99268e4398363f1
demiapollo <apollodemirel67@gmail.com>
2023-05-22 08:57:06 -0700

    connect to jawsdb - cookieParser in server js added

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(session.cookieParser(process.env.SESSION_SECRET));
 app.use(session(sessionConfig));
 
 app.use("/", routes);
